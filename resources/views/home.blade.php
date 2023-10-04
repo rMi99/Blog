ff123448f89f171d8d88683278bd2f0e837b4ac6
rMi99 <99ramigimhama@gmail.com>
2023-10-04 13:06:56 +0530

    blog with migrations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,163 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <!-- PAGE CAPTION
+    ============================================= -->
+    <div class="page-caption">
+        <canvas class="scene subpage scene--full" id="scene"></canvas>
+        <div class="spacer-300 spacer-md-200"></div>
+        <div class="container">
+            <h1 class="title text-anime">The rMeE</h1>
+            <div class="spacer-30 spacer-sm-15"></div>
+            <h6 class="text-anime sub-caption">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+ජීවන සහ සුමිත |
+ Life and Serenity
+
+            </h6>
         </div>
     </div>
-</div>
+
+
+            <!-- NEWS
+            ============================================= -->
+            <div class="news-type-2">
+                <div class="container">
+                    <div class="spacer-150 spacer-md-75"></div>
+                    <div class="row g-5">
+                        <!-- a item -->
+                        <div class="col-md-6 col-12">
+                            <a href="single-news.html" class="news-image image-anime">
+                                <img src="./assets/images/news-1.jpg" alt="">
+                            </a>
+                            <div class="spacer-30"></div>
+                            <div class="categories text-anime">
+                                <a href="#" class="subheading">WEBSITES</a>
+                                <a href="#" class="subheading">BLOGS</a>
+                                <a href="#" class="subheading">MARKETING</a>
+                            </div>
+                            <div class="spacer-15"></div>
+                            <a href="single-news.html"><h6 class="text-anime title">Unleashing the Digital Transformation Power: Amplify Your Online Presence with Digital Agencies</h6></a>
+                            <div class="spacer-30"></div>
+                            <div class="date text-anime">MAY 20, 2022</div>
+                        </div>
+                        <!-- a item -->
+                        <div class="col-md-6 col-12">
+                            <a href="single-news.html" class="news-image image-anime">
+                                <img src="./assets/images/news-2.jpg" alt="">
+                            </a>
+                            <div class="spacer-30"></div>
+                            <div class="categories text-anime">
+                                <a href="#" class="subheading">WEBSITES</a>
+                                <a href="#" class="subheading">BLOGS</a>
+                                <a href="#" class="subheading">MARKETING</a>
+                            </div>
+                            <div class="spacer-15"></div>
+                            <a href="single-news.html"><h6 class="text-anime title">Digital Agencies: Standing Beside Businesses in the Realm of Digital Marketing</h6></a>
+                            <div class="spacer-30"></div>
+                            <div class="date text-anime">MAY 20, 2022</div>
+                        </div>
+                        <!-- a item -->
+                        <div class="col-md-6 col-12">
+                            <a href="single-news.html" class="news-image image-anime">
+                                <img src="./assets/images/news-3.jpg" alt="">
+                            </a>
+                            <div class="spacer-30"></div>
+                            <div class="categories text-anime">
+                                <a href="#" class="subheading">WEBSITES</a>
+                                <a href="#" class="subheading">BLOGS</a>
+                                <a href="#" class="subheading">MARKETING</a>
+                            </div>
+                            <div class="spacer-15"></div>
+                            <a href="single-news.html"><h6 class="text-anime title">Unleashing the Digital Transformation Power: Amplify Your Online Presence with Digital Agencies</h6></a>
+                            <div class="spacer-30"></div>
+                            <div class="date text-anime">MAY 20, 2022</div>
+                        </div>
+                        <!-- a item -->
+                        <div class="col-md-6 col-12">
+                            <a href="single-news.html" class="news-image image-anime">
+                                <img src="assets/images/news-4.jpg" alt="">
+                            </a>
+                            <div class="spacer-30"></div>
+                            <div class="categories text-anime">
+                                <a href="#" class="subheading">WEBSITES</a>
+                                <a href="#" class="subheading">BLOGS</a>
+                                <a href="#" class="subheading">MARKETING</a>
+                            </div>
+                            <div class="spacer-15"></div>
+                            <a href="single-news.html"><h6 class="text-anime title">Digital Agencies: Standing Beside Businesses in the Realm of Digital Marketing</h6></a>
+                            <div class="spacer-30"></div>
+                            <div class="date text-anime">MAY 20, 2022</div>
+                        </div>
+                        <!-- a item -->
+                        <div class="col-md-6 col-12">
+                            <a href="single-news.html" class="news-image image-anime">
+                                <img src="./assets/images/news-5.jpg" alt="">
+                            </a>
+                            <div class="spacer-30"></div>
+                            <div class="categories text-anime">
+                                <a href="#" class="subheading">WEBSITES</a>
+                                <a href="#" class="subheading">BLOGS</a>
+                                <a href="#" class="subheading">MARKETING</a>
+                            </div>
+                            <div class="spacer-15"></div>
+                            <a href="single-news.html"><h6 class="text-anime title">Unleashing the Digital Transformation Power: Amplify Your Online Presence with Digital Agencies</h6></a>
+                            <div class="spacer-30"></div>
+                            <div class="date text-anime">MAY 20, 2022</div>
+                        </div>
+                        <!-- a item -->
+                        <div class="col-md-6 col-12">
+                            <a href="single-news.html" class="news-image image-anime">
+                                <img src="assets/images/news-6.jpg" alt="">
+                            </a>
+                            <div class="spacer-30"></div>
+                            <div class="categories text-anime">
+                                <a href="#" class="subheading">WEBSITES</a>
+                                <a href="#" class="subheading">BLOGS</a>
+                                <a href="#" class="subheading">MARKETING</a>
+                            </div>
+                            <div class="spacer-15"></div>
+                            <a href="single-news.html"><h6 class="text-anime title">Digital Agencies: Standing Beside Businesses in the Realm of Digital Marketing</h6></a>
+                            <div class="spacer-30"></div>
+                            <div class="date text-anime">MAY 20, 2022</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="spacer-150 spacer-md-60 spacer-sm-60"></div>
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-12">
+                            <h3>Let’s create something<br> great together!</h3>
+                        </div>
+                        <div class="col-md-4 col-12 align-right button-wrapper">
+                            <a href="#" class="flen-btn"><span data-hover="hello@flenagency.com">hello@rME.com</span></a>
+                        </div>
+                    </div>
+                    <div class="spacer-30"></div>
+                    <div class="row bottom-wrapper">
+                            <div class="col-xl-4 col-md-12">
+                                <p class="little">Copyright © 2023 rME</p>
+                            </div>
+                            <div class="col-xl-8 col-md-12 social-links">
+                                <a href="#">Facebook</a>
+                                <a href="#">Behance</a>
+                                <a href="#">Dribbble</a>
+                                <a href="#">Instagram</a>
+                            </div>
+                    </div>
+                </div>
+            </footer>
+
+            </main>
+        </div>
+    </div>
+
+
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,162 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- CSRF Token -->
+     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- Fonts  -->
+
+    <link rel="stylesheet" href="{{ asset('assets/fonts/Butler_Stencil_Webfont/stylesheet.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/the_secret/stylesheet.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/Butler_Webfont/stylesheet.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-grid.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+
+    <!-- Font icons -->
+    <link rel="stylesheet" href="{{ asset('assets/cicon-fonts/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/cicon-fonts/remixicon/remixicon.css') }}">
+
 </head>
-<body>
+
+
+<body class="smooth-scroll">
+
+    {{-- preloader bro --}}
+    {{-- <div id="preloader" class="preloader">
+        <!-- <div class="txt">ඉවසන්න...</div> -->
+        <div class="percentage">0%</div>
+        <div class="progress"></div>
+
+    </div> --}}
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="tavonline-overlay"></div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
 
-                    </ul>
+    <div id="cursor">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <i class="ri-arrow-right-up-line"></i>
+        <div class="cursor__bg"></div>
+    </div>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+    <div id="content-scroll" data-scrollbar>
+        <div class="site-wrap" data-barba="wrapper">
+        <main  data-barba="container">
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+        <!-- HEADER
+        ============================================= -->
+        <header>
+            <div class="header-container">
+                <div class="row px-0 gx-0">
+                    <div class="col-xl-2 col-md-6 col-6">
+                        <a href="index.html" class="logo"><img src="assets/images/logo.png"
+                         alt="logo"></a>
+                    </div>
+                    <div class="col-xl-10 col-md-6 col-6">
+                        <div class="burger-menu">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </header>
+
+            <!-- OVERLAY MENU -->
+            <div class="overlay-menu">
+                <div class="close">
+                    <i class="ri-close-line"></i>
+                </div>
+                <div class="row overlay-wrap">
+                    <div class="col-xl-8 col-md-7 left-area">
+                        <nav>
+                            <ul>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Home</a>
+                                    <ul class="sub">
+                                        <li><a href="index.html">Creative Agency</a></li>
+                                        <li><a href="corporate.html">Corporate</a></li>
+                                        <li><a href="creative-portfolio.html">Creative Portfolio</a></li>
+                                        <li><a href="freelancer.html">Freelancer</a></li>
+                                        <li><a href="photographer.html">Photographer</a></li>
+                                        <li><a href="projects-grid.html">Creative Grid</a></li>
+                                        <li><a href="classic-menu.html">Classic Menu</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Portfolio</a>
+                                    <ul class="sub">
+                                        <li><a href="roll-showcase.html">Roll Showcase</a></li>
+                                        <li><a href="cross-showcase.html">Cross Showcase</a></li>
+                                        <li><a href="fullscreen-showcase.html">Fulllscreen Showcase</a></li>
+                                        <li><a href="letterflow-showcase.html">Letter Showcase</a></li>
+                                        <li><a href="half-showcase.html">Half Showcase</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="about.html">About</a></li>
+                                <li class="menu-item-has-children"><a href="#">Journal</a>
+                                    <ul class="sub">
+                                        <li class="current-menu-item"><a href="news-classic.html">Journal Classic</a></li>
+                                        <li><a href="news-list.html">Journal List</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-xl-4 col-md-5 right-area">
+                        <ul>
+                            <li><span class="title">Brief us</span></li>
+                            <li><a href="mailto:hello@flencreative.com" target="_blank">hello@flencreative.com</a></li>
+                            <li><a href="tel:34937606800" target="_blank">Tel. +34 937 606 800</a></li>
+                        </ul>
+                        <ul>
+                            <li><span class="title">Our Office</span></li>
+                            <li><p>Champataz Street , Colombo 9.<br> Sri Lanka</p></li>
+                        </ul>
+                        <ul>
+                            <li><span class="title">Follow us</span></li>
+                            <li>
+                                <ul class="social-links">
+                                    <li><a href="#" target="_blank">Behance</a></li>
+                                    <li><a href="#" target="_blank">Dribbble</a></li>
+                                    <li><a href="#" target="_blank">Instagram</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </main>
+
 
         <main class="py-4">
             @yield('content')
         </main>
+        </div>
     </div>
+
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/webgl.js"></script>
+    {{-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC44qiQKRvAB1ETh4F1oQcSs9PIfLPbCQE&amp;sensor=false"></script> --}}
+    <script src="assets/js/map.js"></script>
+    <script src="assets/js/main.js"></script>
+
 </body>
 </html>
