386aa3c14c3f5730a46a21e7eae9f73a6d7d5a18
rMi99 <99ramigimhama@gmail.com>
2023-10-04 15:32:05 +0530

    non

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('home');
 });
 
-// Auth::routes();
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
